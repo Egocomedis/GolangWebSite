cafff661abcaa1baf78d6b99b5baf1ba2122dcb2
Egocomedis <zender201515@gmail.com>
2021-02-20 13:57:37 +0200

    added bootstrap

--- main.go
+++ main.go
@@ -5,23 +5,24 @@ import ( "fmt" ; "net/http" ; "html/template")
 
 //User is
 type User struct {
-	name string
-	age uint16
-	money int16
-	avgGrades float64 
-	happiness float64
+	Name string
+	Age uint16
+	Money int16
+	AvgGrades float64 
+	Happiness float64
+	Hobbies []string
 }
 
 func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newHame string) {
-	u.name = newHame
+	u.Name = newHame
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
 	//bob.setNewName("Alex")
 	//fmt.Fprint(w, bob.getAllInfo())
 	tmpl, _ := template.ParseFiles("templates/index.html")
